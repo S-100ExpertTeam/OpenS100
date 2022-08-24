0e175a840d6eb106c06d3dd660192c0978155851
JO Gyeongmin <gorogara87@gmail.com>
2022-08-24 13:46:00 +0900

    Delete pc in lua

--- LuaScriptingReference/portrayal_catalog.h
+++ LuaScriptingReference/portrayal_catalog.h
@@ -1,8 +1,9 @@
 #pragma once
-#include "LuaScriptingReference.h"
-#include "luaconf.h"
+
 #include "context_parameter.h"
 
 #include "../PortrayalCatalogue/PortrayalCatalogue.h"
+
+#include <vector>
 
 std::vector<context_parameter> KRS_LUA_SCRIPT_REFERENCE_API pc_get_context_parameters_from_pc(PortrayalCatalogue* pc);
