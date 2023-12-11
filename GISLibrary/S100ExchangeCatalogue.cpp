c70826d5242a69eeb3078f738a6859491baf3bda
isbaek <dlstn8073@naver.com>
2023-12-11 10:49:23 +0900

    commit

--- GISLibrary/S100ExchangeCatalogue.cpp
+++ GISLibrary/S100ExchangeCatalogue.cpp
@@ -1,2 +1,23 @@
 #include "stdafx.h"
-//#include "DataCoverage.h"
+#include "S100ExchangeCatalogue.h"
+
+
+S100ExchangeCatalogue::S100ExchangeCatalogue(void)
+{
+
+}
+
+
+S100ExchangeCatalogue::~S100ExchangeCatalogue(void)
+{
+	for (unsigned int i = 0; i < m_nRecords; i++) {
+		delete m_pSHPObject[i];
+	}
+	delete[] m_pSHPObject;
+}
+
+
+bool SHPFile::Open(CString _filepath) 
+{
+
+}
--- GISLibrary/S100ExchangeCatalogue.h
+++ GISLibrary/S100ExchangeCatalogue.h
@@ -1,1 +1,30 @@
 #pragma once
+#include "spatialobject.h"
+#include "ExchangeCatalogue.h"
+
+#include <memory>
+
+class Geometry;
+class Scaler;
+
+class S100ExchangeCatalogue :
+	public SpatialObject
+{
+public:
+	S100ExchangeCatalogue();
+	virtual ~S100ExchangeCatalogue();
+
+public:
+	unsigned int m_nRecords;					// The number of records
+	unsigned int m_nShapeType;					// Shape Type
+	Geometry** m_pSHPObject;
+	std::shared_prt<ExchangeCatalogue> 
+
+public:
+	/*
+	** Description: Read the file and store the data in the layer.
+	*/
+	bool Open(CString _filepath) override;
+
+	void Draw(HDC& hDC, Scaler* scaler, double offset = 0) override;
+}
