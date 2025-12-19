38c72c9e354c123d16eaf52eb03c71a788c60b40
gorogara <gorogara87@gmail.com>
2025-12-19 22:38:35 +0900

    lua - parsing

--- GISLibrary/CommandList.cpp
+++ GISLibrary/CommandList.cpp
@@ -113,7 +113,64 @@ std::list<S100_Instruction*> CommandList::Parse()
 {
 	std::list<S100_Instruction*> result;
 
+	// State commands
+	// Applicability: All drawing commands
 	std::optional<Part9a::ViewingGroup> viewingGroup;
+	std::optional<Part9a::Id> id;
+
+	// Applicability: All drawing commands, except NullInstruction
+	std::optional<Part9a::DisplayPlane> displayPlane;
+	std::optional<Part9a::DrawingPriority> drawingPriority;
+	std::optional<Part9a::ScaleMinimum> scaleMinimum;
+	std::optional<Part9a::ScaleMaximum> scaleMaximum;
+	std::optional<Part9a::Parent> parent;
+	std::optional<Part9a::Hover> hover;
+
+	// Applicability: PointInstruction, SymbolFill, TextInstruction
+	std::optional<Part9a::LocalOffset> localOffset;
+
+	// Applicability: PointInstruction, TextInstruction
+	std::optional<Part9a::LinePlacement> linePlacement;
+	std::optional<Part9a::AreaPlacement> areaPlacement;
+
+	// Applicability: AreaFillReference, PixmapFill, SymbolFill, HatchFill, TextInstruction
+	std::optional<Part9a::AreaCRS> areaCRS;
+
+	// Applicability: PointInstruction, SymbolFill, TextInstruction, CoverageFill
+	std::optional<Part9a::Rotation> rotation;
+	std::optional<Part9a::Rotation> scaleFactor;
+
+	// Applicability: LineStyle
+	std::optional<Part9a::Dash> dash;
+	std::optional<Part9a::LineSymbol> lineSymbol;
+	
+	// Applicability: LineInstruction, LineInstructionUnsuppressed, HatchFill
+	std::optional<Part9a::LineStyle> lineStyle;
+
+	// Applicability: TextInstruction
+	std::optional<Part9a::FontColor> fontColor;
+
+	// Applicability: TextInstruction, CoverageFill
+	std::optional<Part9a::FontBackgroundColor> fontBackgroundColor;
+	std::optional<Part9a::FontSize> fontSize;
+	std::optional<Part9a::FontProportion> fontProportion;
+	std::optional<Part9a::FontWeight> fontWeight;
+	std::optional<Part9a::FontSlant> fontSlant;
+	std::optional<Part9a::FontSerifs> fontSerifs;
+
+	// Applicability: TextInstruction
+	std::optional<Part9a::FontUnderline> fontUnderline;
+	std::optional<Part9a::FontStrikethrough> fontStrikethrough;
+	std::optional<Part9a::FontUpperline> fontUpperline;
+	std::optional<Part9a::FontReference> fontReference;
+	std::optional<Part9a::TextAlignHorizontal> textAlignHorizontal;
+	std::optional<Part9a::TextAlignVertical> textAlignVertical;
+	std::optional<Part9a::TextVerticalOffset> textVerticalOffset;
+
+	// Applicability: PointInstruction, AreaFillReference, PixmapFill, SymbolFill
+	std::optional<Part9a::OverrideColor> overrideColor;
+	std::optional<Part9a::OverrideAll> overrideAll;
+	std::optional<Part9a::ClearOverride> clearOverride;
 
 	for (auto i = commands.begin(); i != commands.end(); ++i) {
 		auto command = i->get();
